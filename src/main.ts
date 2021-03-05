f2304d87e853b6ecd543fa4b96103f33c6b7c142
Marco Vaccari <marco.vaccari@develondigital.com>
2021-03-05 20:15:09 +0100

    Add settings.

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,35 @@
-import {MarkdownView, Plugin} from 'obsidian'
+import { MarkdownView, Plugin } from 'obsidian'
 import AutocompleteView from './autocomplete'
+import { AutocompleteSettings } from './settings/settings'
+import { AutocompleteSettingsTab } from './settings/settings-tab'
 
 export default class AutocompletePlugin extends Plugin {
   private autocompleteView: AutocompleteView
-  private keyupListener: (cm: CodeMirror.Editor, event: KeyboardEvent) => void
   private lastUsedEditor: CodeMirror.Editor
+  settings: AutocompleteSettings
 
   async onload() {
-    console.log('Loading Obsidian Autocomplete...')
-    this.autocompleteView = new AutocompleteView()
+    console.log('Loading Autocomplete plugin.')
+    this.settings = Object.assign(
+      new AutocompleteSettings(),
+      await this.loadData()
+    )
+
+    this.addSettingTab(new AutocompleteSettingsTab(this.app, this))
+
+    if (!this.settings.enabled) return
+
+    this.autocompleteView = new AutocompleteView(this.settings)
 
     this.addCommand({
       id: 'autocomplete-toggle',
       name: 'Toggle Autocomplete',
-      hotkeys: [{
-        modifiers: ["Ctrl"],
-        key: " "
-      }],
+      hotkeys: [
+        {
+          modifiers: ['Ctrl'],
+          key: ' ',
+        },
+      ],
       callback: () => {
         const editor = this.getCurrentEditor()
 
@@ -28,68 +41,82 @@ export default class AutocompletePlugin extends Plugin {
           // Do not open on vim normal mode
           if (editor.getOption('keyMap') === 'vim') return
 
-          if (autocomplete.isShown())
+          const isEnabled = this.settings.enabled
+          if (autocomplete.isShown() || !isEnabled)
             autocomplete.removeView(editor)
-          else
-            autocomplete.showView(editor)
+          else if (isEnabled) autocomplete.showView(editor)
         }
-      }
+      },
     })
 
-    this.keyupListener = (cm: CodeMirror.Editor, event: KeyboardEvent) => {
-      const autocomplete = this.autocompleteView
+    this.enable()
+  }
 
-      this.updateEditorIfChanged(cm, autocomplete)
+  async onunload() {
+    console.log('Unloaded Obsidian Autocomplete')
+    this.disable()
+  }
 
-      const cursor = cm.getCursor()
-      const currentLineNumber = cursor.line
-      const currentLine: string = cm.getLine(currentLineNumber)
+  async refresh() {
+    this.disable()
+    this.enable()
+  }
 
-
-      // Need to update previous/next state here,
-      // otherwise the view is not updated correctly
-      // (Because I'm trying to cache it)
-      // Missing pattern matching with arrays :(
-      switch (`${event.ctrlKey} ${event.key}`) {
-        case "true p":
-          autocomplete.selectPrevious()
-          break
-        case "true n":
-          autocomplete.selectNext()
-          break
-        case "false ArrowUp":
-          autocomplete.selectPrevious()
-          break
-        case "false ArrowDown":
-          autocomplete.selectNext()
-          break
-      }
-
-      const autocompleteView = autocomplete.getView(currentLine, cm)
-
-      if (autocompleteView)
-        this.appendWidget(cm, autocompleteView)
-
-      autocomplete.scrollToSelected()
-    }
-
-    this.registerCodeMirror(editor => {
+  enable() {
+    this.autocompleteView.loadProviders()
+    this.registerCodeMirror((editor) => {
       editor.on('keyup', this.keyupListener)
     })
   }
 
-  async onunload() {
+  disable() {
     const workspace = this.app.workspace
-    workspace.iterateCodeMirrors(cm => {
+    workspace.iterateCodeMirrors((cm) => {
       cm.off('keyup', this.keyupListener)
       this.autocompleteView.removeView(cm)
     })
-    console.log('Unloaded Obsidian Autocomplete')
   }
 
-  private updateEditorIfChanged(editor: CodeMirror.Editor, autocomplete: AutocompleteView) {
-    if (!this.lastUsedEditor)
-      this.lastUsedEditor = editor
+  private keyupListener = (cm: CodeMirror.Editor, event: KeyboardEvent) => {
+    const autocomplete = this.autocompleteView
+
+    this.updateEditorIfChanged(cm, autocomplete)
+
+    const cursor = cm.getCursor()
+    const currentLineNumber = cursor.line
+    const currentLine: string = cm.getLine(currentLineNumber)
+
+    // Need to update previous/next state here,
+    // otherwise the view is not updated correctly
+    // (Because I'm trying to cache it)
+    // Missing pattern matching with arrays :(
+    switch (`${event.ctrlKey} ${event.key}`) {
+      case 'true p':
+        autocomplete.selectPrevious()
+        break
+      case 'true n':
+        autocomplete.selectNext()
+        break
+      case 'false ArrowUp':
+        autocomplete.selectPrevious()
+        break
+      case 'false ArrowDown':
+        autocomplete.selectNext()
+        break
+    }
+
+    const autocompleteView = autocomplete.getView(currentLine, cm)
+
+    if (autocompleteView) this.appendWidget(cm, autocompleteView)
+
+    autocomplete.scrollToSelected()
+  }
+
+  private updateEditorIfChanged(
+    editor: CodeMirror.Editor,
+    autocomplete: AutocompleteView
+  ) {
+    if (!this.lastUsedEditor) this.lastUsedEditor = editor
 
     if (editor !== this.lastUsedEditor) {
       autocomplete.removeView(this.lastUsedEditor)
@@ -97,10 +124,14 @@ export default class AutocompletePlugin extends Plugin {
     }
   }
 
-  private appendWidget(editor: CodeMirror.Editor, view: HTMLElement, scrollable = true) {
+  private appendWidget(
+    editor: CodeMirror.Editor,
+    view: HTMLElement,
+    scrollable = true
+  ) {
     const cursor = editor.getCursor()
 
-    editor.addWidget({ch: cursor.ch, line: cursor.line}, view, scrollable)
+    editor.addWidget({ ch: cursor.ch, line: cursor.line }, view, scrollable)
   }
 
   private getCurrentEditor(): CodeMirror.Editor | null {
