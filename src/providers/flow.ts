61c01ca03b2f285de795a0242e589ccc041b84d8
Yeboster <yeboster@gmail.com>
2021-03-12 19:35:32 +0100

    Improve word recognition in flow provider.

--- src/providers/flow.ts
+++ src/providers/flow.ts
@@ -5,23 +5,46 @@ export class FlowProvider extends Provider {
   completions = []
 
   addCompletionWord(line: string, cursorIndex: number): void {
-    const word = this.getLastWordFrom(line, cursorIndex)
+    const { normalized, updatedCursor } = this.normalizedLine(line, cursorIndex)
+
+    const word = this.getLastWordFrom(normalized, updatedCursor)
 
     if (!word || this.alreadyAdded(word)) return
 
     this.completions.push(word)
   }
 
+  private normalizedLine(
+    line: string,
+    cursorIndex: number
+  ): { normalized: string; updatedCursor: number } {
+    const partialLine = line.slice(0, cursorIndex)
+    let normalized = partialLine.trimEnd()
+
+    // Subtract how many spaces removed
+    let updatedCursor = cursorIndex - (partialLine.length - normalized.length)
+
+    if (normalized.length === 0) return { normalized: '', updatedCursor: 0 }
+
+    const lastChar = normalized.charAt(updatedCursor - 1)
+
+    if (Provider.wordSeparatorRegex.test(lastChar)) {
+      updatedCursor -= 1
+      normalized = normalized.slice(0, updatedCursor)
+    }
+
+    return { normalized, updatedCursor }
+  }
+
   private getLastWordFrom(line: string, cursorIndex: number): string | null {
     let wordStartIndex = cursorIndex
     const wordRegex = /[\w$]+/
-    while (wordStartIndex && wordRegex.test(line.charAt(wordStartIndex - 1))) {
+    while (wordStartIndex && wordRegex.test(line.charAt(wordStartIndex - 1)))
       wordStartIndex -= 1
-    }
 
     let word: string | null = null
     if (wordStartIndex !== cursorIndex)
-      word = line.slice(wordStartIndex, cursorIndex).trimRight()
+      word = line.slice(wordStartIndex, cursorIndex)
 
     return word
   }
