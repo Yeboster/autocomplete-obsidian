86f641f123c3e4771a61fb78a86b378a012618d6
Yeboster <yeboster@gmail.com>
2021-03-20 12:36:07 +0100

    Improve sort order for suggestions.

--- src/providers/provider.ts
+++ src/providers/provider.ts
@@ -6,18 +6,36 @@ export abstract class Provider {
   static readonly placeholder: string = '#{}'
 
   matchWith(input: string): Completion[] {
-    // TODO: Improve filtering with weights
-    const suggestions = this.completions.filter(val => val.includes(input))
-      .map(sugg => {
-        return {category: this.category, value: sugg}
+    const inputLowered = input.toLowerCase()
+    const inputHasUpperCase = /[A-Z]/.test(input)
+
+    // TODO: Show lowercase first and the upper case, if search is case insensitive
+
+    const suggestions = this.completions
+      .filter((suggestion) => {
+        // case-sensitive logic if input has an upper case.
+        // Otherwise, uses case-insensitive logic
+        let included: boolean
+        if (inputHasUpperCase) included = suggestion.includes(input)
+        else included = suggestion.toLowerCase().includes(inputLowered)
+
+        return included
       })
-      .sort((a, b) => a.value.length - b.value.length)
+      .sort((a, b) => a.length - b.length)
+      .sort(
+        (a, b) =>
+          Number(b.toLowerCase().startsWith(inputLowered)) -
+          Number(a.toLowerCase().startsWith(inputLowered))
+      )
+      .map((suggestion) => {
+        return { category: this.category, value: suggestion }
+      })
 
     return suggestions
   }
 }
 
 export interface Completion {
-  category: string,
+  category: string
   value: string
 }
