8411e8402f1b34c71c75d8c9dad192a0acb620e2
Yeboster <yeboster@gmail.com>
2021-03-21 12:21:25 +0100

    Improve sort suggestions.

--- src/providers/provider.ts
+++ src/providers/provider.ts
@@ -9,8 +9,6 @@ export abstract class Provider {
     const inputLowered = input.toLowerCase()
     const inputHasUpperCase = /[A-Z]/.test(input)
 
-    // TODO: Show lowercase first and the upper case, if search is case insensitive
-
     // case-sensitive logic if input has an upper case.
     // Otherwise, uses case-insensitive logic
     const suggestions = this.completions
@@ -19,12 +17,7 @@ export abstract class Provider {
           ? suggestion.includes(input)
           : suggestion.toLowerCase().includes(inputLowered)
       )
-      .sort((a, b) => a.length - b.length)
-      .sort(
-        (a, b) =>
-          Number(b.toLowerCase().startsWith(inputLowered)) -
-          Number(a.toLowerCase().startsWith(inputLowered))
-      )
+      .sort((a,b) => a.localeCompare(b))
       .map((suggestion) => {
         return { category: this.category, value: suggestion }
       })
